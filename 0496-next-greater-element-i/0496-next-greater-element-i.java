d60285bdff51af325ad1efc118ecd8b1d9801e6e
Kalyani Garud <kggarud27@gmail.com>
2026-08-21 23:52:57 +0530

    Time: 7 ms (8.48%), Space: 44.7 MB (90.91%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int res []=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+           res[i]=-1;
+            for(int j=0;j<nums2.length-1;j++){
+                if(nums1[i]==nums2[j]){
+                    for(int k=j+1;k<nums2.length;k++){
+                       if(nums2[j]<nums2[k]){
+                           res[i]=nums2[k];
+                        break;
+                        }
+                    }
+                    
+                }
+            } 
+        } return res;
+    }
+}
